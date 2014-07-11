ed8519138e74f122f8d865f05e0a32842e2ba92e
diogofriggo <diogofriggobr@gmail.com>
2014-07-11 15:56:28 -0300

    Mistankengly created VB instead of C#

---
+++ RequestManagement/Data/ServiceRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RequestManagement.Data
+{
+    public class ServiceRequest
+    {
+        public User Requester;
+        public string Description;
+        
+    }
+}
--- RequestManagement/Global.asax
+++ RequestManagement/Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.vb" Inherits="RequestManagement.MvcApplication" Language="VB" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RequestManagement.MvcApplication" Language="C#" %>
