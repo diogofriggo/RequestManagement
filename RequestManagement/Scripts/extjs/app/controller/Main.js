74a898d1f8d7b2610b200b0688852ae84a3af277
diogofriggo <diogofriggobr@gmail.com>
2014-07-14 17:31:39 -0300

    Added extjs Part I

---
+++ RequestManagement/Scripts/extjs/app/controller/Main.js
@@ -0,0 +1,3 @@
+Ext.define('SM.controller.Main', {
+    extend: 'Ext.app.Controller'
+});
---
+++ RequestManagement/Scripts/extjs/build/temp/production/SM/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require C:\Users\Diogo\Documents\GitHub\RequestManagement\RequestManagement\Scripts\app\app.js
